a755e65b968426967718634a5fabcd06acf8e1a5
Shubham Jadhav <shubhamjadhv3182@gmail.com>
2023-07-28 11:18:56 +0530

    state in class based components

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let{title, description} = this.props;
+    let{title, description, imageUrl,} = this.props;
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-  <img src="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/B361/production/_130512954_benstokes.jpg" className="card-img-top" alt="..."/>
+  <img src={imageUrl} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
-    <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+    <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
   </div>
 </div>
       </div>
