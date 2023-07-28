5ac46698184aa15ce15095ec054f1d126912fac5
Shubham Jadhav <shubhamjadhv3182@gmail.com>
2023-07-28 14:38:13 +0530

    Fetch API in react to populate NewsItems

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -6,10 +6,10 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-  <img src={imageUrl} className="card-img-top" alt="..."/>
+  <img src={!imageUrl?"https://images.hindustantimes.com/tech/img/2023/07/27/1600x900/Meteorite_1690439443530_1690439443836.jpg":imageUrl} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">{title}...</h5>
-    <p className="card-text">{description}...</p>
+    <h5 className="card-title">{title}</h5>
+    <p className="card-text">{description}</p>
     <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
   </div>
 </div>
